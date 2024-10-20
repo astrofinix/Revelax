bdb0ef9b7f6245780c07a069a480062283aab8dd
Paul Gapud <campusinvasionproject@gmail.com>
2024-10-21 00:01:12 +0800

    init stylings

--- revelax/src/App.svelte
+++ revelax/src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello, {name}!</h1>
+	<p>Welcome to Revelax</p>
 </main>
 
 <style>
@@ -15,12 +15,12 @@
 		margin: 0 auto;
 	}
 
-	h1 {
+	/* h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
+	} */
 
 	@media (min-width: 640px) {
 		main {
