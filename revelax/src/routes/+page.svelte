cf3c565ebe3b2cfffae2df5bd023bd123e511305
Paul Gapud <campusinvasionproject@gmail.com>
2024-10-21 01:44:02 +0800

    reinit svelte - reinitialize Svelte in consideration of shadcn frontend ui - reason for reinit: frontend components cannot be installed on the main-branch initialization

---
+++ revelax/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name = "World";
+
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<main>
+	<h1>Hello, {name}!</h1>
+	<p>Welcome to Revelax</p>
+
+  <Button>Create Room</Button>
+
+  <Button>Join Room</Button>
+</main>
