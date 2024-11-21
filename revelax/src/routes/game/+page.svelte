3af863ef1a149a523a473f3cb9778fca0d5f75bc
Paul Gapud <campusinvasionproject@gmail.com>
2024-11-21 08:19:37 +0800

    push work from previous repo

---
+++ revelax/src/routes/game/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Gradient from "$lib/components/gradient.svelte";
+</script>
+
+<main>
+  <Gradient />
+
+  <div class="content">
+    <div class="aether-h1">Revelax</div>
+    <h1>(Game Page)</h1>
+
+  </div>
+</main>
